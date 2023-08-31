e73944e1850b33da7db2e62213addf5eafd27bc5
Fabian Frank Werner <fabianfrankwerner@gmail.com>
2023-08-31 14:11:23 +0200

    Add function playRound with every possible outcome, then restarts the game

--- script.js
+++ script.js
@@ -8,3 +8,53 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+const playerSelection = prompt("Rock, Paper or Scissors? Choose wisely: ").toLowerCase();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+        if (playerSelection == "rock" && computerSelection == "scissors") {
+            alert("You win! Rock beats Scissors.");
+            location.reload();
+            return;
+        } else if (playerSelection == "scissors" && computerSelection == "rock") {
+            alert("You lose! Rock beats Scissors.");
+            location.reload();
+            return;
+        } else if (playerSelection == "scissors" && computerSelection == "paper") {
+            alert("You win! Scissors beats Paper.");
+            location.reload();
+            return;
+        } else if (playerSelection == "paper" && computerSelection == "scissors") {
+            alert("You lose! Scissors beats Paper.");
+            location.reload();
+            return;
+        } else if (playerSelection == "paper" && computerSelection == "rock") {
+            alert("You win! Paper beats Rock.");
+            location.reload();
+            return;
+        } else if (playerSelection == "rock" && computerSelection == "paper") {
+            alert("You lose! Paper beats Rock.");
+            location.reload();
+            return;
+        } else if (playerSelection == "rock" && computerSelection == "rock") {
+            alert("Tie! Rock ties Rock.");
+            location.reload();
+            return;
+        } else if (playerSelection == "paper" && computerSelection == "paper") {
+            alert("Tie! Paper ties Paper.");
+            location.reload();
+            return;
+        } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+            alert("Tie! Scissors ties Scissors.");
+            location.reload();
+            return;
+        } 
+    } else {
+        alert("Please choose rock, paper or scissors!");
+        location.reload();
+        return;
+    }
+}
+playRound(playerSelection, computerSelection);
